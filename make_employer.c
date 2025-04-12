02c59b0f0c44d084ea8c9646d80181e0ec6795b2
probrahim <ibrahimhassani674@gmail.com>
2025-04-12 13:27:12 +0100

    insert_point

--- make_employer.c
+++ make_employer.c
@@ -4,12 +4,17 @@
 
 
 void make_employer(){
+    Employer n;
+    printf("entrer vore identite :");
+    scanf("%d",&n.id);
+    printf("inserer le nom :");
+    scanf("%s",n.nom);
+    printf("inserer le prenom :");
+    scanf("%s",n.prenom);
+    printf("inserer salaire :");
+    scanf("%f",&n.s);
 
+    printf("voila votre nom : %s \t voila votre prenom   : %s",n.nom,n.prenom);
     
-}
 
-
-int main()
-{
-    make_employer();
 }
