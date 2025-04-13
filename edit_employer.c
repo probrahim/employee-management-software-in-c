4fe71b3e09fe8710c2324b31165092158938fddb
probrahim <ibrahimhassani674@gmail.com>
2025-04-13 16:22:19 +0100

    edit_employer

---
+++ edit_employer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "shell.h"
+
+void edit_employer() {
+    int id;
+    printf("=============== > Entrez l'ID de l'employé à modifier : ");
+    scanf("%d", &id);
+
+    char filename[100];
+    sprintf(filename, "employe_%d.txt", id);
+
+    printf("----  Ouverture de fichier %s  :  ", filename);
+
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        printf("\nErreur : Fichier %s non trouvé.\n", filename);
+        return;
+    } else {
+        printf("Fichier %s ouvert avec succès.\n", filename);
+    }
+
+    Employer d;
+    if (fscanf(f, "%d %s %s %f", &d.id, d.nom, d.prenom, &d.s) != 4) {
+        printf("Erreur lors de la lecture des données.\n");
+        fclose(f);
+        return;
+    }
+
+
+}
