65a41ecb5cfad167780cb3ce6e0f7f4ee52834a7
probrahim <ibrahimhassani674@gmail.com>
2025-04-15 22:50:12 +0100

    v0

--- del_employer.c
+++ del_employer.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "shell.h"
-
+#include <stdbool.h>
 
 
 void del_employer(){
     int id;
+    int v =  0;
+
+
+
+
     printf("entre matricule \t :");
     scanf("%d",&id);
 
+    char filename[100];
 
+    sprintf(filename, "employe_%d.txt",id);
+    FILE *f = fopen(filename,"r");
+
+    if (f == NULL){
+        printf("non trouve");
+    }
 
 }
